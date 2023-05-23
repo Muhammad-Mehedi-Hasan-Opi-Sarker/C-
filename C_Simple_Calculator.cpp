1282e1cea91fa89dfd0312056574342fefa6cb75
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-23 09:32:51 +0600

    added simple calculator

---
+++ C_Simple_Calculator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    cout << X << " + " << Y << " = " << X + Y << endl;
+    cout << X << " * " << Y << " = " << static_cast<long long>(X) * Y << endl;
+    cout << X << " - " << Y << " = " << X - Y << endl;
+    return 0;
+}
